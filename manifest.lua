9f2008098bf36fdcbe729bcda4a302ebd8f638c5
HellBreezMix <49804948+HellBreezMix@users.noreply.github.com>
2026-07-30 06:10:49 +0300

    Update manifest.lua

---
+++ manifest.lua
@@ -0,0 +1,16 @@
+--------------------------------------------------
+-- BlackJack
+-- manifest.lua
+--------------------------------------------------
+
+return {
+
+    name = "BlackJack",
+
+    version = "1.0.0",
+
+    author = "hellbreez",
+
+    build = "stable"
+
+}
